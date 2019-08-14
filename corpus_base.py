0b55d46dda6c35810f34d81e436c4d6a745cac64
vgratian <vgratian@utopianlab.am>
2019-08-14 21:12:26 +0200

    first working version with fake intent parser and slot filler (randomly generated probabilities)

--- corpus_base.py
+++ corpus_base.py
@@ -7,6 +7,8 @@ class Corpus:
         self.type_req = type_req
         self.__corpus = list()
         self.__curr = 0 # counter for iterator
+        self.intent_labels = {}
+        self.slot_labels = []
 
 
     def __next__(self):
@@ -20,18 +22,33 @@ class Corpus:
     def __iter__(self):
         return iter(self.__corpus)
 
+    def get_labels(self):
+        if not self.intent_labels or not self.slot_labels:
+            for x in self.__corpus:
+                intent = x.get_gold_intent()
+                slots = x.get_gold_slots()
+                if intent not in self.intent_labels:
+                    self.intent_labels[intent] = []
+                for slot in slots:
+                    if slot not in self.intent_labels[intent]:
+                        self.intent_labels[intent].append(slot)
+                    if slot not in self.slot_labels:
+                        self.slot_labels.append(slot)
+
+        return self.intent_labels, self.slot_labels
+
 
     def get_data(self):
-        
+
         data = list()
 
         files = ['slp_train_add_to_playlist.txt', 'slp_train_book_restaurant.txt', 'slp_train_get_weather.txt', 'slp_train_play_music.txt', 'slp_train_rate_book.txt', 'slp_train_search_creative_work.txt', 'slp_train_search_screening_event.txt']
-        
+
         for each in files:
-            f = open(each,'r') 
+            f = open(each,'r')
             lines = f.readlines()
-            
-            
+
+
             # ------ check if train or test data is needed and set the list accordingly ------ #
             if self.type_req == 'train':
                 print('Collecting train data ...')
@@ -39,7 +56,7 @@ class Corpus:
                 print('Collecting test data ...')
                 lines = reversed(lines)
 
-            
+
             # ----- start loop to fill up data in the data structure ----- #
             i = 0
             for line in lines:
@@ -54,7 +71,7 @@ class Corpus:
                     # ---- make an instance of the corpus class ---- #
                     q = Query(utterance, intent, slots)
                     self.__corpus.append(q)
-                    i+=1    
+                    i+=1
 
     # def operate(self):
     #     return self.get_data()
@@ -64,5 +81,3 @@ class Corpus:
 # c.operate()
 
 # print(c.operate())
-        
-
--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -6,31 +6,32 @@ class Evaluator():
 
         self.intents = I
         self.slots = S
+        self.corpus = X
 
         # Print headers on console
-        self.print_headers(I, S, X)
+        #self.print_headers(I, S, X)
 
         # Calculate TP, FP, FN, P, R, F1 for individual classes
-        self.get_intent_scores(I, S, X)
-        self.get_slot_scores(I, S, X)
+        self.get_intent_scores()
+        self.get_slot_scores()
 
         # Calculate F1 Macro and F1 Micro for model
-        self.get_model_scores(I, S)
+        self.get_model_scores()
 
 
-    def self.get_intent_scores(self, I, X):
+    def get_intent_scores(self):
 
         ## Initialize scores with 0s
-        self.intent_tp = {i:0 for i in I}
-        self.intent_fp = {i:0 for i in I}
-        self.intent_fn = {i:0 for i in I}
-        self.intent_precision = {i:0 for i in I}
-        self.intent_recall = {i:0 for i in I}
-        self.intent_f1 = {i:0 for i in I}
+        self.intent_tp = {i:0 for i in self.intents}
+        self.intent_fp = {i:0 for i in self.intents}
+        self.intent_fn = {i:0 for i in self.intents}
+        self.intent_precision = {i:0 for i in self.intents}
+        self.intent_recall = {i:0 for i in self.intents}
+        self.intent_f1 = {i:0 for i in self.intents}
 
         # Calculate base scores: TP, FP and FN
-        for x in X:
-            for i in I:
+        for x in self.corpus:
+            for i in self.intents:
                 if x.get_pred_intent() == i:
                     # predicted intent is true
                     if x.get_gold_intent() == i:
@@ -43,10 +44,10 @@ class Evaluator():
                     self.intent_fn[i] += 1
 
         # Calculate precision, recall, local F score
-        for i in I:
+        for i in self.intents:
             # precision = tp / (tp + fp)
             tp_fp = self.intent_tp[i] + self.intent_fp[i]
-            p = self.intent_tp / tp_fp if tp_fp else 0
+            p = self.intent_tp[i] / tp_fp if tp_fp else 0
 
             # recall = tp / (tp + fn)
             tp_fn = self.intent_tp[i] + self.intent_fn[i]
@@ -60,18 +61,19 @@ class Evaluator():
             self.intent_recall[i] = r
             self.intent_f1[i] = f1
 
-    def self.get_slot_scores(self, S, X):
+
+    def get_slot_scores(self):
 
         ## Initialize base scores for slot values
-        self.slot_tp = {s:0 for s in S}
-        self.slot_fp = {s:0 for s in S}
-        self.slot_fn = {s:0 for s in S}
-        self.slot_precision = {s:0 for s in S}
-        self.slot_recall = {s:0 for s in S}
-        self.slot_f1 = {s:0 for s in S}
+        self.slot_tp = {s:0 for s in self.slots}
+        self.slot_fp = {s:0 for s in self.slots}
+        self.slot_fn = {s:0 for s in self.slots}
+        self.slot_precision = {s:0 for s in self.slots}
+        self.slot_recall = {s:0 for s in self.slots}
+        self.slot_f1 = {s:0 for s in self.slots}
 
         # Calculate base scores: TP, FP and FN
-        for x in X:
+        for x in self.corpus:
             gold = x.get_gold_slots()
             pred = x.get_pred_slots()
             # Iterate gold slots
@@ -88,14 +90,14 @@ class Evaluator():
                     self.slot_fp[s] += 1
 
         # Calculate precision, recall, local F score
-        for s in S:
+        for s in self.slots:
             # precision = tp / (tp + fp)
             tp_fp = self.slot_tp[s] + self.slot_fp[s]
             p = self.slot_tp / tp_fp if tp_fp else 0
 
             # recall = tp / (tp + fn)
             tp_fn = self.slot_tp[s] + self.slot_fn[s]
-            r = self.intent_tp[s] / tp_fn if tp_fn else 0
+            r = self.slot_tp[s] / tp_fn if tp_fn else 0
 
             # f1 score = 2pr / p+r
             f1 = (2*p*r) / (p+r) if (p+r) else 0
@@ -106,11 +108,11 @@ class Evaluator():
             self.slot_f1[s] = f1
 
 
-    def self.get_model_scores(self, I, S):
+    def get_model_scores(self):
 
         # F1-Macro is just average of all class F1 scores
         self.intent_f1['macro'] = sum( self.intent_f1.values() ) / len( self.intent_f1 )
-        self.intent_f1['macro'] = sum( self.slot_f1.values() ) / len( self.slot_f1 )
+        self.slot_f1['macro'] = sum( self.slot_f1.values() ) / len( self.slot_f1 )
 
         # F1-Micro is calculated from base scores of classes
         # Calculate for intent
@@ -129,6 +131,7 @@ class Evaluator():
         r = tp / (fp + fn) if (tp + fn) else 0
         self.slot_f1['micro'] = (1*p*r) / (p*r) if (p*r) else 0
 
+
     def __str__(self, use_bold=True):
         """
         Fancy list of macro, micro f-scores and precision and recall for all labels
@@ -137,7 +140,7 @@ class Evaluator():
         unbold = '\033[0m' if use_bold else ''
 
         # Macro and Micro F1 scores of intent prediction
-        intent_sum = '{}{}  {}  {}  {}{}\t'.format(
+        intent_sum = '{}{}  {}{}\t'.format(
                         bold,
                         round(self.intent_f1['macro'], 2),
                         round(self.intent_f1['micro'], 2),
@@ -151,17 +154,17 @@ class Evaluator():
                 for i in self.intents )
 
         # Macro and Micro F1 scores of slot filling
-        slot_sum = '{}{}  {}  {}  {}{}\t'.format(
+        slot_sum = '{}{}  {}{}\t'.format(
                         bold,
                         round(self.slot_f1['macro'], 2),
                         round(self.slot_f1['micro'], 2),
                         unbold  )
 
         # F1, Precision and Recall for each intent
-        intent_scores = '\t'.join('{} {} {}'.format(
+        slot_scores = '\t'.join('{} {} {}'.format(
                         round(self.slot_f1[s], 2),
                         round(self.slot_precision[s], 2),
                         round(self.slot_recall[s], 2) )
                 for s in self.slots )
 
-        return all_scores
+        return ''.join([intent_sum, intent_scores, slot_sum, slot_scores])
