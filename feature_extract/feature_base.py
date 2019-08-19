551b4dbe68a5f0a82357538ed2a8c886ad6270eb
Xan192 <sengupta.meghdut@gmail.com>
2019-08-19 18:29:56 +0200

    Added functions for n-grams incorporating tfidfs

--- feature_extract/feature_base.py
+++ feature_extract/feature_base.py
@@ -5,8 +5,9 @@ sys.path.append('../corpus')
 from corpus_base import Corpus
 
 sys.path.append('../utils')
-import helper
+from helper import *
 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class Feature:
     def __init__(self, __data_dump):
@@ -14,22 +15,48 @@ class Feature:
         self.__intents = ['AddToPlaylist', 'BookRestaurant', 'GetWeather', 'PlayMusic', 'RateBook', 'SearchCreativeWork', 'SearhScreeningEvent']
         self.__vocab_set = set()
 
+        self.create_tfidf(self.__data_dump)
+
     def make_vocabs(self):
         for data in self.__data_dump:
-            tokens = tokenize(data['utterance'])
+            tokens = tokenize(data)
             for token in tokens:
                 self.__vocab_set.add(token)
 
-        return self.__vocab_set
+        # return self.__vocab_set
 
-    
-    
-corpus = Corpus(200, 'train')
+    def create_tfidf(self, __all_sents):
+        # self.__all_vocabs = __all_vocabs
+        self.__all_sents = __all_sents
+
+        # # ---- Taking into account just the term frequencies:
+        # vectorizer = CountVectorizer(ngram_range=(2,2))
+        # # ---- The ngram range specifies the ngram configuration.
+
+        # X = vectorizer.fit_transform(self.__all_sents)
+        
+        # # ---- Testing the ngram generation:
+        # # ---------------- #
+        # print(vectorizer.get_feature_names())
+        # # ---------------- #
+        # print(X.toarray())
+
+        vectorizer = TfidfVectorizer(ngram_range=(2,2)) # You can still specify n-grams here.
+        X = vectorizer.fit_transform(self.__all_sents)
+
+        # # ---- Testing the TFIDF value + ngrams:
+        print(X.toarray())
+
+corpus = Corpus(3, 'train')
 corpus.get_data()
 
+all_sent = list()
+for inst in corpus:
+    all_sent.append(inst.get_utterance())
+
+feature = Feature(all_sent)
 
 
 
-
-# res = f.make_vocabs()
+# res = feature.make_vocabs()
 # print(res)
