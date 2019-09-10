56d2a59ccde0ab0ea963876890f5652d066abe81
vgratian <vgratian@utopianlab.am>
2019-09-10 20:34:20 +0200

    trying to simplify how svm is run, seperate corpora as train and test

--- feature_extract/feature_base.py
+++ feature_extract/feature_base.py
@@ -8,10 +8,11 @@ from models.baseline_svm_intent import SVM
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class Feature:
-    def __init__(self):
+    def __init__(self, vocab):
         # self.__data_dump = __data_dump
         self.__intents = ['AddToPlaylist', 'BookRestaurant', 'GetWeather', 'PlayMusic', 'RateBook', 'SearchCreativeWork', 'SearhScreeningEvent']
-        self.__vocab_set = set()
+        self.__vocab_set = vocab
+        self.__vectorizer = TfidfVectorizer(ngram_range=(1,1), vocabulary=self.__vocab_set) # You can still specify n-grams here.
 
         # self.create_tfidf(self.__data_dump)
 
@@ -39,19 +40,22 @@ class Feature:
         # # ---------------- #
         # print(X.toarray())
 
-        vectorizer = TfidfVectorizer(ngram_range=(2,2)) # You can still specify n-grams here.
-        X = vectorizer.fit_transform(self.__all_sents)
-
+        X = self.__vectorizer.fit_transform(self.__all_sents)
+        print('tfidfvectorizer vocab: ', len(self.__vectorizer.vocabulary))
+        print('tfidfvectorizer vocab_: ', len(self.__vectorizer.vocabulary_))
+        print('TYPE OF X: ', type(X))
+        print('SHAPE OF X: ', X.shape)
         # # ---- Testing the TFIDF value + ngrams:
         # print(X.toarray())
 
         return X.toarray()
 
-corpus_tr = Corpus(1950, 'train')
-corpus_tr.get_data()
+corpus_tr = Corpus(1500, 'train')
+corpus_tr.shuffle()
 
-corpus_ts = Corpus(30, 'test')
-corpus_ts.get_data()
+corpus_ts = Corpus(200, 'test')
+corpus_ts.shuffle()
+
 
 # ---- grab all utterances ----
 all_sent_tr = list()
@@ -63,14 +67,18 @@ all_sent_ts = list()
 for inst in corpus_ts:
     all_sent_ts.append(inst.get_utterance())
 
-# ---- train feature ----
-feature_tr = Feature()
+all_sent_combined = all_sent_tr + all_sent_ts
+vocab = set()
+for sent in all_sent_combined:
+    vocab.update(sent.split())
 
-feature_tr = feature_tr.create_tfidf(all_sent_tr)
+# ---- train feature ----
+feat = Feature(vocab)
+
+feature_tr = feat.create_tfidf(all_sent_tr)
 
 # ---- test feature ---- 
-feature_ts = Feature()
-feature_ts = feature_ts.create_tfidf(all_sent_ts)
+feature_ts = feat.create_tfidf(all_sent_ts)
 
 # print(feature_ts.create_tfidf(all_sent_ts))
 
@@ -94,6 +102,6 @@ for inst in corpus_ts:
 
 # ------------- enter all data and all target as input to SVM ----------------- #
 
-baseline_svm = SVM(feature_tr, all_targ_tr)
+baseline_svm = SVM(feature_tr, all_targ_tr, feature_ts, all_targ_ts)
 
 baseline_svm.train()
--- models/baseline_svm_intent.py
+++ models/baseline_svm_intent.py
@@ -6,37 +6,52 @@ from sklearn.metrics import classification_report
 
 
 class SVM:
-    def __init__(self, data, targets):
-        self.data = data # assuming that the things are coming in fine 
-        self.targets = targets
+    def __init__(self, train_data, train_targets, test_data, test_targets):
+        self.train_data = train_data
+        self.train_targets = train_targets
+        self.test_data = test_data
+        self.test_targets = test_targets
 
     def train(self):
         
         # --------- transform targets ----------- # 
         le = preprocessing.LabelEncoder()
-        self.__targets = self.targets  
-        self.__targets = le.fit_transform(self.__targets)
         
         # ---------- make train test split --------- #
-        X_train, X_test, y_train, y_test = train_test_split(self.data, self.__targets, test_size=0.3)
+        #X_train, X_test, y_train, y_test = train_test_split(self.data, self.__targets, test_size=0.3)
 
         # ---------- BUILD SVM MODEL ------------ #
-        clf = svm.SVC(kernel='linear') # Linear Kernel
+        clf = svm.SVC(  C = 10,
+                        kernel = 'poly', 
+                        verbose = True,
+                        gamma = 0.00000000000000001,
+                        probability=True) # Linear Kernel
 
         # ---------- Train the model using the training sets ------------ #
-        clf.fit(X_train, y_train)
+        clf.fit(self.train_data, le.fit_transform(self.train_targets))
 
         # ----------- Predict the response for test dataset ------------ #
-        y_pred = clf.predict(X_test)
+        y_pred = clf.predict(self.test_data)
         
         # ---------- MODEL ACCURACY ----------- #  
-        print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+        print()
+        print("Accuracy:",metrics.accuracy_score(le.fit_transform(self.test_targets), y_pred))
 
         # Model Precision: what percentage of positive tuples are labeled as such?
-        print("Precision:",metrics.precision_score(y_test, y_pred, average='micro'))
+        print("Precision:",metrics.precision_score(le.fit_transform(self.test_targets), y_pred, average='micro'))
 
         # # Model Recall: what percentage of positive tuples are labelled as such?
-        # print("Recall:",metrics.recall_score(y_test, y_pred))
+        print("Recall:",metrics.recall_score(le.fit_transform(self.test_targets), y_pred, average='micro'))
 
         print("\n")
-        print(classification_report(y_test, y_pred))
+        print(classification_report(le.fit_transform(self.test_targets), y_pred))
+
+
+        print('TRAIN DATA')
+        #for i in range(7):
+        #    print(i,le.fit_transform(self.train_targets).count(i))    
+
+        print('TEST DATA')
+        print(y_pred)
+        print()
+        print(le.fit_transform(self.test_targets))
