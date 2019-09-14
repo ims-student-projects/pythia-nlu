381d829e9c8b2d7e2c147d225530325dc13bfeeb
Xan192 <sengupta.meghdut@gmail.com>
2019-09-14 17:59:38 +0200

    added code for retaining probabilities

--- feature_extract/feature_base.py
+++ feature_extract/feature_base.py
@@ -2,9 +2,9 @@
 # ---- make vocabs from corpus base ---- #
 import sys 
 sys.path.append(sys.path[0] + '/../')
-from corpus.corpus_base import Corpus
-from utils.helper import *
-from models.baseline_svm_intent import SVM
+# from corpus.corpus_base import Corpus
+# from utils.helper import *
+# from models.baseline_svm_intent import SVM
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class Feature:
@@ -26,7 +26,7 @@ class Feature:
 
     def create_tfidf(self, __all_sents):
         # self.__all_vocabs = __all_vocabs
-        self.__all_sents = __all_sents
+        self.__all_sents = __all_sents  
 
         # # ---- Taking into account just the term frequencies:
         # vectorizer = CountVectorizer(ngram_range=(2,2))
@@ -50,58 +50,4 @@ class Feature:
 
         return X.toarray()
 
-corpus_tr = Corpus(1500, 'train')
-corpus_tr.shuffle()
 
-corpus_ts = Corpus(200, 'test')
-corpus_ts.shuffle()
-
-
-# ---- grab all utterances ----
-all_sent_tr = list()
-for inst in corpus_tr:
-    all_sent_tr.append(inst.get_utterance())
-
-# ---- grab all utterances ----
-all_sent_ts = list()
-for inst in corpus_ts:
-    all_sent_ts.append(inst.get_utterance())
-
-all_sent_combined = all_sent_tr + all_sent_ts
-vocab = set()
-for sent in all_sent_combined:
-    vocab.update(sent.split())
-
-# ---- train feature ----
-feat = Feature(vocab)
-
-feature_tr = feat.create_tfidf(all_sent_tr)
-
-# ---- test feature ---- 
-feature_ts = feat.create_tfidf(all_sent_ts)
-
-# print(feature_ts.create_tfidf(all_sent_ts))
-
-# ---- grab all train targets ----
-all_targ_tr = list()
-for inst in corpus_tr:
-    all_targ_tr.append(inst.get_gold_intent())
-
-# print(all_targ_tr)
-
-# ---- grab all test targets ----
-all_targ_ts = list()
-for inst in corpus_ts:
-    all_targ_ts.append(inst.get_gold_intent())
-
-
-# --------------------------------------- #
-
-# all_data = feature_tr.append(feature_ts)
-# all_targ = all_targ_tr + all_targ_ts
-
-# ------------- enter all data and all target as input to SVM ----------------- #
-
-baseline_svm = SVM(feature_tr, all_targ_tr, feature_ts, all_targ_ts)
-
-baseline_svm.train()
--- models/baseline_svm_intent.py
+++ models/baseline_svm_intent.py
@@ -1,3 +1,8 @@
+import sys 
+sys.path.append(sys.path[0] + '/../')
+from corpus.corpus_base import Corpus
+from feature_extract.feature_base import Feature
+
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn import svm
@@ -6,14 +11,15 @@ from sklearn.metrics import classification_report
 
 
 class SVM:
-    def __init__(self, train_data, train_targets, test_data, test_targets):
+    def __init__(self, train_num):
+        self.train_num = train_num
+        self.__predicted = []
+
+    def train(self, train_data, train_targets, test_data, test_targets):
         self.train_data = train_data
         self.train_targets = train_targets
         self.test_data = test_data
         self.test_targets = test_targets
-
-    def train(self):
-        
         # --------- transform targets ----------- # 
         le = preprocessing.LabelEncoder()
         
@@ -25,16 +31,32 @@ class SVM:
                         kernel = 'poly', 
                         verbose = True,
                         gamma = 0.00000000000000001,
-                        probability=True) # Linear Kernel
+                        probability=True) 
 
         # ---------- Train the model using the training sets ------------ #
         clf.fit(self.train_data, le.fit_transform(self.train_targets))
+
+        # ---------- Retain probabilities ------------------------------- #
+        self.__predicted = clf.predict_proba(self.test_data)
+        
+        __prbList = list()
+
+        for p in self.__predicted:
+            probs = {}
+            for i in range(len(p)):
+                probs[le.inverse_transform([i])[0]] = p[i]
+                __prbList.append(probs)
+        
+        for i, j in zip(__prbList, self.corpus_ts):
+            j.set_intent_probabilities(i)
+
+        print('----------- Intent probabilities set is complete ---------------')
 
         # ----------- Predict the response for test dataset ------------ #
         y_pred = clf.predict(self.test_data)
         
         # ---------- MODEL ACCURACY ----------- #  
-        print()
+        
         print("Accuracy:",metrics.accuracy_score(le.fit_transform(self.test_targets), y_pred))
 
         # Model Precision: what percentage of positive tuples are labeled as such?
@@ -55,3 +77,71 @@ class SVM:
         print(y_pred)
         print()
         print(le.fit_transform(self.test_targets))
+
+        # predictions_test = le.inverse_transform(y_pred)
+        # print(predictions_test)
+
+        # 'AddToPlaylist' 0 'BookRestaurant' 1 'get weather' 2 'PlayMusic'3 
+        # 'RateBook' 4 'SearchCreativeWork' 5 'SearchScreeningEvent' 6 
+
+    def setup(self):
+
+        self.corpus_tr = Corpus(self.train_num, 'train')
+        self.corpus_tr.shuffle()
+
+        self.corpus_ts = Corpus(60, 'test')
+        self.corpus_ts.shuffle()
+
+
+        # ---- grab all utterances ----
+        self.all_sent_tr = list()
+        for inst in self.corpus_tr:
+            self.all_sent_tr.append(inst.get_utterance())
+
+        # ---- grab all utterances ----
+        self.all_sent_ts = list()
+        for inst in self.corpus_ts:
+            self.all_sent_ts.append(inst.get_utterance())
+
+        self.all_sent_combined = self.all_sent_tr + self.all_sent_ts
+        self.vocab = set()
+        for sent in self.all_sent_combined:
+            self.vocab.update(sent.split())
+
+        # ---- train feature ----
+        feat = Feature(self.vocab)
+
+        self.feature_tr = feat.create_tfidf(self.all_sent_tr)
+
+        # ---- test feature ---- 
+        self.feature_ts = feat.create_tfidf(self.all_sent_ts)
+
+        # print(feature_ts.create_tfidf(all_sent_ts))
+
+        # ---- grab all train targets ----
+        self.all_targ_tr = list()
+        for inst in self.corpus_tr:
+            self.all_targ_tr.append(inst.get_gold_intent())
+
+        # print(all_targ_tr)
+
+        # ---- grab all test targets ----
+        self.all_targ_ts = list()
+        for inst in self.corpus_ts:
+            self.all_targ_ts.append(inst.get_gold_intent())
+
+
+        # --------------------------------------- #
+
+        # all_data = feature_tr.append(feature_ts)
+        # all_targ = all_targ_tr + all_targ_ts
+
+# ------------- enter all data and all target as input to SVM ----------------- #
+
+if __name__ == '__main__':
+
+    baseline_svm = SVM(100)
+
+    baseline_svm.setup()
+
+    baseline_svm.train(baseline_svm.feature_tr, baseline_svm.all_targ_tr, baseline_svm.feature_ts, baseline_svm.all_targ_ts)
